1eb831ce7951bb37232d723bdb7c5e2455e4b901
yasinduramanayake <yasinduramanayake123@gmail.com>
2021-09-18 15:20:53 +0530

    updated

---
+++ src/views/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <ion-page>
+    <img class="logo" src="assets/frame.png" />
+  </ion-page>
+</template>
+<script>
+import { IonPage } from "@ionic/vue";
+export default {
+  components: {
+    IonPage,
+  },
+};
+</script>
