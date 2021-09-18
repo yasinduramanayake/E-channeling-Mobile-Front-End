eaef367f1f80e7faaea5ccd66cd66247c4a61add
yasinduramanayake <yasinduramanayake123@gmail.com>
2021-09-18 22:11:59 +0530

    updated

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,41 @@
 <template>
   <ion-page>
-    <ion-content class="background-image" no-scroll padding> </ion-content>
+    <ion-content class="background-image" no-scroll padding>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <center>
+        <img src="assets/download.png" />
+      </center>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div style="padding-left:80px;padding-right:80px">
+        <ion-button size="large" expand="full">Login </ion-button>
+        <br />
+        <ion-button size="large" expand="full">Register </ion-button>
+      </div>
+    </ion-content>
   </ion-page>
 </template>
 <script>
-import { IonPage, IonContent } from "@ionic/vue";
+import { IonPage, IonContent, IonButton } from "@ionic/vue";
 export default {
   components: {
     IonPage,
     IonContent,
+    IonButton,
   },
 };
 </script>
 <style scoped>
-ion-content {
-  --background: #03fede;
-}
+@import "../theme/custom.css";
 </style>
