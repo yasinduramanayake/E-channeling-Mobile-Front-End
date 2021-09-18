cd19f990842f400c0ccb549a8cbc062fe1495459
yasinduramanayake <yasinduramanayake123@gmail.com>
2021-09-18 16:06:37 +0530

    updated

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,21 +1,19 @@
 <template>
   <ion-page>
-    <img class="logo" src="assets/frame.png" />
-
-    <ion-content>
-      <ion-card>
-        <img class="logo" src="assets/download.png" />
-      </ion-card>
-    </ion-content>
+    <ion-content class="background-image" no-scroll padding> </ion-content>
   </ion-page>
 </template>
 <script>
-import { IonPage, IonContent, IonCard } from "@ionic/vue";
+import { IonPage, IonContent } from "@ionic/vue";
 export default {
   components: {
     IonPage,
     IonContent,
-    IonCard,
   },
 };
 </script>
+<style scoped>
+ion-content {
+  --background: #03fede;
+}
+</style>
